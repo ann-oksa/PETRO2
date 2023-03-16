e58b77426aa8881e76f35719dfd4bc5e81a7a2e7
Hanna <oksanichenko@magora.co.uk>
2023-03-16 13:48:54 +0100

    Initial Commit

---
+++ PETRO2/PETRO2App.swift
@@ -0,0 +1,17 @@
+//
+//  PETRO2App.swift
+//  PETRO2
+//
+//  Created by mac on 16.03.2023.
+//
+
+import SwiftUI
+
+@main
+struct PETRO2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
